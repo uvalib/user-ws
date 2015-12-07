583d56c6a48f1dd49192a7a748a4e7c00bbdc28d
dpg3k <dpg3k@d-137-155-231.bootp.Virginia.EDU>
2015-12-07 15:55:54 -0500

    Moving packages around

---
+++ src/github.com/uvadave/user-ws/response.go
@@ -0,0 +1,10 @@
+package main
+
+//import "time"
+
+type Response struct {
+   Status        int     `json:"status"`
+   Message       string  `json:"message"`
+   User          User    `json:"user,omitempty"`
+}
+
