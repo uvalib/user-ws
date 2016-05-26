0398f0ade02710c767a923653971ff6640976657
UVADave <dpg3k@virginia.edu>
2016-05-26 11:57:14 -0400

    Added environment controlled startup options

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,1 +1,19 @@
-bin/user-ws
+# set blank options variables
+LDAPURL_OPT=""
+TOKENURL_OPT=""
+
+# public LDAP hostname:port
+if [ -n "$LDAP_URL" ]; then
+   LDAPURL_OPT="--url $LDAP_URL"
+fi
+
+# token authentication service URL
+if [ -n "$TOKENAUTH_URL" ]; then
+   TOKENURL_OPT="--tokenauth $TOKENAUTH_URL"
+fi
+
+bin/user-ws $LDAPURL_OPT $TOKENURL_OPT
+
+#
+# end of file
+#
