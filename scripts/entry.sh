b2dba846ae0dd65f062ddb06a15443023f2e2ddb
UVADave <dpg3k@virginia.edu>
2016-08-05 10:44:01 -0400

    Add env controlled debug support; service does not use this yet

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,6 +1,7 @@
 # set blank options variables
 LDAPURL_OPT=""
 TOKENURL_OPT=""
+DEBUG_OPT=""
 
 # public LDAP hostname:port
 if [ -n "$LDAP_URL" ]; then
@@ -10,6 +11,11 @@ fi
 # token authentication service URL
 if [ -n "$TOKENAUTH_URL" ]; then
    TOKENURL_OPT="--tokenauth $TOKENAUTH_URL"
+fi
+
+# service debugging
+if [ -n "$USERINFO_DEBUG" ]; then
+   DEBUG_OPT="--debug"
 fi
 
 bin/user-ws $LDAPURL_OPT $TOKENURL_OPT
