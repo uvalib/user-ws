f368cb0854854fdbb1caf21cb22b364c0a4074c6
UVADave <dpg3k@virginia.edu>
2016-10-27 08:39:15 -0400

    Pull healthcheck user from the environment

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,6 +1,7 @@
 # set blank options variables
 LDAPURL_OPT=""
 TOKENURL_OPT=""
+HEALTHCHECK_OPT=""
 DEBUG_OPT=""
 
 # public LDAP hostname:port
@@ -13,12 +14,17 @@ if [ -n "$TOKENAUTH_URL" ]; then
    TOKENURL_OPT="--tokenauth $TOKENAUTH_URL"
 fi
 
+# healthcheck username
+if [ -n "$HEALTHCHECK_USER" ]; then
+   HEALTHCHECK_OPT="--hcuser $HEALTHCHECK_USER"
+fi
+
 # service debugging
 if [ -n "$USERINFO_DEBUG" ]; then
    DEBUG_OPT="--debug"
 fi
 
-bin/user-ws $LDAPURL_OPT $TOKENURL_OPT $DEBUG_OPT
+bin/user-ws $LDAPURL_OPT $HEALTHCHECK_OPT $TOKENURL_OPT $DEBUG_OPT
 
 #
 # end of file
