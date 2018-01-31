d6bbb5e8e7899bd4f593ca5bbaf15c3ea7c8e73d
UVADave <dpg3k@virginia.edu>
2018-01-31 11:15:53 -0500

    Fix typo

--- src/userws/router.go
+++ src/userws/router.go
@@ -66,7 +66,7 @@ func NewRouter() *mux.Router {
 			Handler(handler)
 	}
 
-	// add the route for the expvars eendpoint
+	// add the route for the expvars endpoint
 	router.Handle("/debug/vars", http.DefaultServeMux )
 
 	return router
