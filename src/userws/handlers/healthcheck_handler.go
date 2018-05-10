d993ed371ca1738f596d7a4547be1147d17c8976
UVADave <dpg3k@virginia.edu>
2018-05-10 12:25:18 -0400

    Log whenever the healthcheck fails

--- src/userws/handlers/healthcheck_handler.go
+++ src/userws/handlers/healthcheck_handler.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 	"userws/config"
 	"userws/ldap"
+	"userws/logger"
+	"fmt"
 )
 
 //
@@ -21,8 +23,10 @@ func HealthCheck(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		healthy = false
 		message = err.Error()
+		logger.Log(fmt.Sprintf( "ERROR: LDAP lookup reports '%s'", message ) )
 	} else if user == nil {
 		healthy = false
+		logger.Log(fmt.Sprintf( "ERROR: LDAP lookup cannot find '%s'", config.Configuration.HealthCheckUser ) )
 	}
 
 	encodeHealthCheckResponse(w, healthy, message)
